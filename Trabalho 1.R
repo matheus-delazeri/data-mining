216cd354cc7162a3f55d71d39bf2362dd00cccb2
Matheus Delazeri <mdsouza@inf.ufsm.br>
2024-10-22 10:17:39 -0300

    Feat: added apriori call with example supp and conf

--- Trabalho 1.R
+++ Trabalho 1.R
@@ -1,1 +1,24 @@
-library('jsonlite')
+library(jsonlite)
+library(tidyr)
+library(dplyr)
+library(arules)
+
+json <- fromJSON('https://raw.githubusercontent.com/matheus-delazeri/data-mining/refs/heads/main/padaria_trab.json')
+
+dataset <- json %>%
+  # Define um ID incremental no lugar dos IDs fixos
+  mutate(compra = row_number()) %>%
+  # Separa os produtos em rows individuais
+  unnest(produtos)
+  # Remove o restante da string após o primeiro espaço em branco (para generalizar o produto)
+  #mutate(produtos = sapply(produtos, function(x) strsplit(x, " ")[[1]][1]))
+
+trans <- as(split(dataset$produtos, dataset$compra), "transactions")
+inspect(trans)
+
+rules <- apriori(trans, parameter = list(supp = 0.04, conf = 0.2, minlen=2))
+
+inspect(rules)
+
+subset = subset(rules, subset = (rhs %pin% 'Doce'))
+inspect(subset)
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 ## Correção dos dados
 - Linha 737: JSON inválido (vírgula faltando)
+- Linha 176: Produtos agrupados 'Pastel Presunto e Queijo'
